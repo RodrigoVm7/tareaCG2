9a1a8edef5029b80b8f40adba34263256b03e6e7
rodrigovm7 <rodrigovm7@gmail.com>
2020-07-29 16:08:48 -0400

    cubierto primer caso donde solo hay un maximo en la dilatacion

--- Tarea2.m
+++ Tarea2.m
@@ -14,12 +14,12 @@ classdef Tarea2 < handle
             [eex, eey] = size(ee); 
             eex = floor(eex/2);
             eey = floor(eey/2);
-            %A?adido de bordes a la imagen original. Resultado => Ireplica.
-            Ireplica = ones(tamx+eex*2, tamy+eey*2, 3)*255;
+            %Anadido de bordes a la imagen original. Resultado => Ireplica.
+            Ireplica = zeros(tamx+eex*2, tamy+eey*2, 3);
             for c=1:canal
                 Ireplica(1+eex:tamx+eex, 1+eey:tamy+eey, c) = double(obj.imOriginal(:,:,c));
             end
-            %C?lculo distancias euclideanas de cada p?xel al origen. Resultado => Idistancias.
+            %Calculo distancias euclideanas de cada pixel al origen. Resultado => Idistancias.
             Idistancias = zeros(tamx+eex*2,tamy+eey*2);
             for x=1:tamx+eex*2
                 for y=1:tamy+eey*2
@@ -27,7 +27,25 @@ classdef Tarea2 < handle
                 end
             end     
             %obj.imProcesada = uint8(Ireplica);
-            obj.imProcesada = Idistancias;
+            %obj.imProcesada = Idistancias;
+            
+            Idilatada = zeros(tamx, tamy, 3);
+            for x=1+eex:tamx+eex
+                for y=1+eey:tamy+eey
+                    ventana = Idistancias(x-eex:x+eex, y-eey:y+eey);
+                    ventana = ventana.*ee;
+                    minimo = max(ventana(:));
+                    [cordX, cordY] = find(ventana == minimo);
+                    if(numel(cordX) == 1) %Caso cuando solo hay un solo minimo.
+                        Idilatada(x-eex, y-eex, 1) = obj.imOriginal(cordX(1), cordY(1), 1);
+                        Idilatada(x-eex, y-eex, 2) = obj.imOriginal(cordX(1), cordY(1), 2);
+                        Idilatada(x-eex, y-eex, 3) = obj.imOriginal(cordX(1), cordY(1), 3);
+                    else %Caso en que hay mas de un minimo.
+                        %Usar otras metricas. Proyeccion Ortogonal -
+                        %Distancia al objetivo.
+                    end
+                end
+            end
         end
         
         
